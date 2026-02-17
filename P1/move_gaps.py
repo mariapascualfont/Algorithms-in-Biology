5611be91ab6f74410425b1b399b36b3c0296e616
mariapascualfont <maria.pascual.font@estudiantat.upc.edu>
2026-02-17 10:51:34 +0100

    Remove docstring and print statements from move_gaps
    Removed docstring and example calls from move_gaps function.

--- P1/move_gaps.py
+++ P1/move_gaps.py
@@ -1,16 +1,4 @@
 def move_gaps(seq1, seq2):
-    """
-    Generate alignments by moving gaps in one sequence
-    in another sequence.
-    >>> move_gaps("THEFASTCAT", "THEFATCAT")
-    ['THEFASTCAT', '-THEFATCAT', 'T-HEFATCAT', 'TH-EFATCAT', 'THE-FATCAT', 'THEF-ATCAT', 'THEFA-TCAT', 'THEFAT-CAT', 'THEFATC-AT', 'THEFATCA-T', 'THEFATCAT-']
-    >>> move_gaps("THEFASTCAT", "AFASTCAT")
-    ['THEFASTCAT', '--AFASTCAT', 'A--FASTCAT', 'AF--ASTCAT', 'AFA--STCAT', 'AFAS--TCAT', 'AFAST--CAT', 'AFASTC--AT', 'AFASTCA--T', 'AFASTCAT--']
-    >>> move_gaps("THEFATCAT", "THEFASTCAT")
-    ['THEFASTCAT', '-THEFATCAT', 'T-HEFATCAT', 'TH-EFATCAT', 'THE-FATCAT', 'THEF-ATCAT', 'THEFA-TCAT', 'THEFAT-CAT', 'THEFATC-AT', 'THEFATCA-T', 'THEFATCAT-']
-    >>> move_gaps("AFASTCAT", "THEFASTCAT")
-    ['THEFASTCAT', '--AFASTCAT', 'A--FASTCAT', 'AF--ASTCAT', 'AFA--STCAT', 'AFAS--TCAT', 'AFAST--CAT', 'AFASTC--AT', 'AFASTCA--T', 'AFASTCAT--']
-    """
     if len(seq2) > len(seq1):
       seq1, seq2 = seq2, seq1
     gaps = (len(seq1) - len(seq2))* '-'
@@ -21,6 +9,3 @@ def move_gaps(seq1, seq2):
       filled_gap = head+gaps+tail
       result.append(filled_gap)
     return result
-
-print(move_gaps("THEFASTCAT", "THEFATCAT"))
-print(move_gaps("AFASTCAT", "THEFASTCAT"))
