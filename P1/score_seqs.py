8bbb62a560bdc8d57b0f9a4a311c542d8dc499f1
mariapascualfont <maria.pascual.font@estudiantat.upc.edu>
2026-02-17 10:51:04 +0100

    Clean up score_seqs function by removing comments
    Removed docstring and example calls from score_seqs function.

--- P1/score_seqs.py
+++ P1/score_seqs.py
@@ -1,20 +1,5 @@
 def score_seqs(seq1, seq2, match, mismatch, gap):
-    """
-    Calculate the score of two sequences based on a matching score,
-    a mismatching score, and a gap penalty.
-
-    >>> score_seqs("THEFASTCAT", "THEFATCAT-", 1, -1, -2)
-    -1
-    >>> score_seqs("THEFASTCAT", "THEFATCA-T", 1, -1, -2)
-    1
-    >>> score_seqs("THEFA-TCAT", "THEFASTCAT", 1, -1, -2)
-    7
-    >>> score_seqs("THEFASTCAT", "THE", 1, -1, -2)
-    >>> score_seqs("THE-FASTCAT", "THE-FASTCAT", 1, -1, -2)
-    10
-    """
     if len(seq1) != len(seq2):
-      print("The sequences are not of the same length.")
       return 0
     score = 0
     for i in range(len(seq1)):
@@ -26,9 +11,3 @@ def score_seqs(seq1, seq2, match, mismatch, gap):
       else:
         score += mismatch
     return score
-    
-print(score_seqs("THEFASTCAT", "THEFATCAT-", 1, -1, -2))
-print(score_seqs("THEFASTCAT", "THEFATCA-T", 1, -1, -2))
-print(score_seqs("THEFA-TCAT", "THEFASTCAT", 1, -1, -2))
-print(score_seqs("THEFASTCAT", "THE", 1, -1, -2))
-print(score_seqs("THE-FASTCAT", "THE-FASTCAT", 1, -1, -2))
